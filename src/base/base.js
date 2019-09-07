0f214621cc879e1b08e516d435b0290fe5b2b81c
Nidhin Joseph <nidhin.joseph1@gmail.com>
2019-09-07 17:07:23 +1000

    add nanoevents module for events

--- src/base/base.js
+++ src/base/base.js
@@ -1,4 +1,5 @@
 import { Background } from '../background/background';
+import NanoEvents from 'nanoevents';
 
 export class NJPicker {
 
@@ -7,6 +8,8 @@ export class NJPicker {
             throw ('NJPicker requires a target element(targetEl) or target element ID(targetID)');
         }
         this.config = Object.assign(this.defaultConfig, options); // merge options with default config
+        this.checkTarget(); // checks for a valid target element
+        this.emitter = new NanoEvents();
         this.build(); // build the picker
     }
     // plugin default config
@@ -34,25 +37,30 @@ export class NJPicker {
     }
     // set the options for building the plugin
     build() {
-        this.checkTarget(); // checks for a valid target element
         this.targetElement.addEventListener('click', this.showPicker.bind(this)); // attach the click to show picker
 
         this.container = document.createElement('div'); // create picker container div
         this.container.classList.add('nj-picker'); // add the container class name
-        this.overlay = new Background(); // init the backdrop
+        this.overlay = new Background(); // init the overlay
 
-        this.container.append(this.bg.build()); // append the backdrop to the picker container
+        this.container.append(this.overlay.build()); // append the overlay to the picker container
         document.body.append(this.container); // attach the picker container to the dom
-    } 
+        this.emitter.emit('ready'); // emit the plugin ready event
+    }
 
     // shows the picker
     showPicker() {
-        this.overlay.show(); // shows the backdrop
+        this.overlay.show(); // shows the overlay
     }
 
     // hides the picker
     hidePicker() {
         this.overlay.hide(); // hides the backdrop
+    }
+
+    // 
+    on(name, value) {
+        this.emitter.on(name, value);
     }
 
 }
