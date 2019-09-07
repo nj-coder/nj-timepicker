8a1605f25b0fb4b3ed33bfd346ee04585ac7f2f4
Nidhin Joseph <nidhin.joseph1@gmail.com>
2019-09-07 18:08:34 +1000

    added clickOverlayClose config

--- src/background/background.js
+++ src/background/background.js
@@ -1,9 +1,19 @@
+import NanoEvents from 'nanoevents';
+
 export class Background {
+
+    constructor(config) {
+        this.config = config;
+        this.emitter = new NanoEvents();
+    }
 
     // build
     build() {
         this.element = document.createElement('div');
         this.element.className = 'nj-overlay';
+        this.element.addEventListener('click', () => {
+            this.emitter.emit('overlay-click');
+        });
         return this.element; // return the element to parent
     }
 
@@ -16,5 +26,8 @@ export class Background {
     hide() {
         this.element.classList.remove('nj-overlay-show');
     }
-
+    // create an on method to mask emitter on
+    on(name, value) {
+        this.emitter.on(name, value);
+    }
 }
--- src/base/base.js
+++ src/base/base.js
@@ -7,8 +7,11 @@ export class NJPicker {
         if (!options.targetEl && !options.targetID) {
             throw ('NJPicker requires a target element(targetEl) or target element ID(targetID)');
         }
-        this.config = Object.assign(this.defaultConfig, options); // merge options with default config
-        this.checkTarget(); // checks for a valid target element
+        // merge options with default config
+        this.config = Object.assign(this.defaultConfig, options);
+        // checks for a valid target element
+        this.checkTarget();
+
         this.emitter = new NanoEvents();
         this.build(); // build the picker
     }
@@ -17,8 +20,7 @@ export class NJPicker {
         return {
             targetEl: null,
             targetID: null,
-            format: '12',
-            theming: {}
+            clickOverlayClose: true
         };
     }
     // check if the target element exist in the dom
@@ -37,28 +39,41 @@ export class NJPicker {
     }
     // set the options for building the plugin
     build() {
-        this.targetElement.addEventListener('click', this.showPicker.bind(this)); // attach the click to show picker
+        // attach the click to show picker
+        this.targetElement.addEventListener('click', this.showPicker.bind(this));
 
-        this.container = document.createElement('div'); // create picker container div
-        this.container.classList.add('nj-picker'); // add the container class name
-        this.overlay = new Background(); // init the overlay
+        // create picker container div
+        this.container = document.createElement('div');
+        this.container.classList.add('nj-picker');
 
-        this.container.append(this.overlay.build()); // append the overlay to the picker container
-        document.body.append(this.container); // attach the picker container to the dom
+        // init the overlay
+        this.overlay = new Background(this.config);
+        if (this.config.clickOverlayClose) {
+            this.overlay.on('overlay-click', this.hidePicker.bind(this));
+        }
+
+        // append the overlay to the picker container
+        this.container.append(this.overlay.build(this));
+        // attach the picker container to the dom
+        document.body.append(this.container);
         this.emitter.emit('ready'); // emit the plugin ready event
     }
 
     // shows the picker
     showPicker() {
+        this.emitter.emit('before.show'); // emit the before pluging show event
         this.overlay.show(); // shows the overlay
+        this.emitter.emit('show'); // emit plugin show event
     }
 
     // hides the picker
     hidePicker() {
+        this.emitter.emit('hide'); // emit the plugin hide event
         this.overlay.hide(); // hides the backdrop
+        this.emitter.emit('after.hide'); // emit the plugin after hide event
     }
 
-    // 
+    // create an on method to mask emitter on
     on(name, value) {
         this.emitter.on(name, value);
     }
