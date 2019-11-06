9f1276c3a0719cb80e7671ca561d072f40762627
Nidhin Joseph <nidhin.joseph1@gmail.com>
2019-11-07 10:17:14 +1100

    version bump

--- src/index.js
+++ src/index.js
@@ -200,5 +200,5 @@ export class NJTimePicker {
 
 if (typeof global === 'object' && !global.NJTimePicker) {
     global.NJTimePicker = NJTimePicker;
-    global.NJTimePicker.version = 'v1.2.101';
+    global.NJTimePicker.version = 'v1.2.102';
 }
