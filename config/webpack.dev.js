5deae7fde80abaf8e2906f8577f76bcc4a34b4ad
Nidhin Joseph <nidhin.joseph1@gmail.com>
2019-11-24 16:25:27 +1100

    updated webpack to support umd

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -5,6 +5,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   output: {
+    libraryTarget: 'umd',
     filename: 'scripts.js'
   },
   devServer: {
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,6 +4,7 @@ const common = require('./webpack.config');
 module.exports = merge(common, {
   mode: 'production',
   output: {
+    libraryTarget: 'umd',
     filename: 'scripts.min.js'
   }
 });
